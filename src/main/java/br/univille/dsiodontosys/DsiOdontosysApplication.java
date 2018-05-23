4eb32f0bb6a44eca7fb283dc11df3c318a9ae393
MendesTK <murilo.begnini@univille.br>
2018-05-23 11:37:21 -0300

    commit

---
+++ src/main/java/br/univille/dsiodontosys/DsiOdontosysApplication.java
@@ -0,0 +1,12 @@
+package br.univille.dsiodontosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsiOdontosysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsiOdontosysApplication.class, args);
+	}
+}
