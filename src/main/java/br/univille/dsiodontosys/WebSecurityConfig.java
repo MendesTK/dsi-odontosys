d5027e84ee145dac04adf69c6a508c5dd89e5ce3
1111007520 <1111007520@LAB-C326-17.univille.br>
2018-09-10 20:23:14 -0300

    security

--- src/main/java/br/univille/dsiodontosys/WebSecurityConfig.java
+++ src/main/java/br/univille/dsiodontosys/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package br.univille.dsiodontosys;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
@@ -21,7 +23,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic().and().formLogin().permitAll();
+		String[] resources = new String[] { "/", "/home", "/pictureCheckCode", "/include/**", "/css/**", "/icons/**",
+				"/images/**", "/js/**", "/layer/**" };
+
+		http.authorizeRequests().anyRequest().authenticated().antMatchers(resources).permitAll().and().formLogin()
+				.loginPage("/login").successForwardUrl("/").permitAll();
 	}
 
 	@Bean
